1f7e275e2e5e7d3346c46e202d55ad7186777a16
Mohamed Yusuf <vitzeno@gmail.com>
2019-04-06 20:50:01 +0100

    Fix bug where valid sites with invalid RSS would cause crash on staert up, now user is given choice to remove said link at start up

--- src/utilities/RSSParser.java
+++ src/utilities/RSSParser.java
@@ -5,14 +5,14 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-
+import java.util.Optional;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.XMLEvent;
-
+import database.DatabaseHandler;
 import feed.Feed;
 import feed.FeedItem;
 import javafx.scene.control.Alert;
@@ -87,93 +87,120 @@ public class RSSParser {
      * This method parses the RSS from the provided URL, it uses the Streaming API for XML (StAX) API to parse the RSS
      * feed which is in XML format
      * @return a feed object which contains a list of feed items or null
+     * @throws XMLStreamException 
      */
     public Feed readFeed() {
-        Feed feed = null;
-        try {
-            boolean isFeedHeader = true;
-            String description = "";
-            String title = "";
-            String link = "";
-            String language = "";
-            String copyright = "";
-            String author = "";
-            String pubdate = "";
-            String guid = "";
-            String url = this.url.toString();
-
-            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-            InputStream in = read();
-            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+        Feed feed = new Feed();
+        
+        boolean isFeedHeader = true;
+        String description = "";
+        String title = "";
+        String link = "";
+        String language = "";
+        String copyright = "";
+        String author = "";
+        String pubdate = "";
+        String guid = "";
+        String url = this.url.toString();
+        
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        InputStream in = read();
+        
             
-            
-            while(eventReader.hasNext()) {
-            	XMLEvent event = eventReader.nextEvent();
-      
-            	switch(event.getEventType()) {
-            		//Read opening tag of XML
-            		case XMLStreamConstants.START_ELEMENT:
-            			String name = event.asStartElement().getName().getLocalPart();
-            			//Deals with the feed header
-            			if(name.equalsIgnoreCase(ITEM) || name.equalsIgnoreCase(ENTRY)) {
-            				if (isFeedHeader) {
-                                isFeedHeader = false;
-                                feed = new Feed(title, link, description, language, copyright, pubdate, url);
+        XMLEventReader eventReader = null;
+		try {
+			eventReader = inputFactory.createXMLEventReader(in);
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+        
+        if(!(eventReader == null)) {
+        	//System.out.println("event reader not null");
+        	while(eventReader.hasNext()) {
+            	//System.out.println("event reader has next");
+            	try {
+                	XMLEvent event = eventReader.nextEvent();
+          
+                	switch(event.getEventType()) {
+                		//Read opening tag of XML
+                		case XMLStreamConstants.START_ELEMENT:
+                			String name = event.asStartElement().getName().getLocalPart();
+                			//Deals with the feed header
+                			if(name.equalsIgnoreCase(ITEM) || name.equalsIgnoreCase(ENTRY)) {
+                				if (isFeedHeader) {
+                                    isFeedHeader = false;
+                                    feed = new Feed(title, link, description, language, copyright, pubdate, url);
+                                }
+                                //event = eventReader.nextEvent();
+                			}
+                			
+                			//Deals with each tag name
+                			switch (name) {
+                            case TITLE:
+                                title = getCharacterData(event, eventReader);
+                                break;
+                            case DESCRIPTION:
+                                description = getCharacterData(event, eventReader);
+                                break;
+                            case LINK:
+                                link = getCharacterData(event, eventReader);
+                                break;
+                            case GUID:
+                                guid = getCharacterData(event, eventReader);
+                                break;
+                            case LANGUAGE:
+                                language = getCharacterData(event, eventReader);
+                                break;
+                            case AUTHOR:
+                                author = getCharacterData(event, eventReader);
+                                break;
+                            case PUB_DATE:
+                                pubdate = getCharacterData(event, eventReader);
+                                break;
+                            case COPYRIGHT:
+                                copyright = getCharacterData(event, eventReader);
+                                break;
                             }
-                            //event = eventReader.nextEvent();
-            			}
-            			
-            			//Deals with each tag name
-            			switch (name) {
-                        case TITLE:
-                            title = getCharacterData(event, eventReader);
-                            break;
-                        case DESCRIPTION:
-                            description = getCharacterData(event, eventReader);
-                            break;
-                        case LINK:
-                            link = getCharacterData(event, eventReader);
-                            break;
-                        case GUID:
-                            guid = getCharacterData(event, eventReader);
-                            break;
-                        case LANGUAGE:
-                            language = getCharacterData(event, eventReader);
-                            break;
-                        case AUTHOR:
-                            author = getCharacterData(event, eventReader);
-                            break;
-                        case PUB_DATE:
-                            pubdate = getCharacterData(event, eventReader);
-                            break;
-                        case COPYRIGHT:
-                            copyright = getCharacterData(event, eventReader);
-                            break;
-                        }
-            			break;
-            		
-            		//Read closing tag of XML
-            		case XMLStreamConstants.END_ELEMENT:
-            			if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
-                            FeedItem message = new FeedItem();
-                            message.setAuthor(author);
-                            message.setDescription(description);
-                            message.setGuid(guid);
-                            message.setLink(link);
-                            message.setTitle(title);
-                            message.setPubDate(pubdate);
-                            feed.getMessages().add(message);
-                            //event = eventReader.nextEvent();
-                            //continue;
-                        }
-            			break;
-            	}
-            }
-        } catch (XMLStreamException e) {
-        	
-        	e.printStackTrace();
-            throw new RuntimeException(e);
+                			break;
+                		
+                		//Read closing tag of XML
+                		case XMLStreamConstants.END_ELEMENT:
+                			if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
+                                FeedItem item = new FeedItem();
+                                item.setAuthor(author);
+                                item.setDescription(description);
+                                item.setGuid(guid);
+                                item.setLink(link);
+                                item.setTitle(title);
+                                item.setPubDate(pubdate);
+                                feed.getMessages().add(item);
+                                //event = eventReader.nextEvent();
+                                //continue;
+                            }
+                			break;
+                	}
+                } catch (XMLStreamException e ) {
+                	e.printStackTrace();
+                	
+                	DatabaseHandler DBHandler = new DatabaseHandler();
+                	String toDelete = this.url.toString();
+                	
+                	Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                	alert.setTitle("Error");
+                	alert.setContentText("Error regarding:   " + toDelete + "\n" 
+                				+ "\n"
+                				+ "It is causing parsing issues, it may not be a valid RSS feed, do you wish to remove it?");
+                	Optional<ButtonType> answer = alert.showAndWait();
+                	
+                	if(answer.get() == ButtonType.OK) {
+                		DBHandler.deleteFromFeedTable(toDelete);
+                	}
+        			
+        			return feed;
+                }
+            }     	
         }
+         
         return feed;
     }
     
